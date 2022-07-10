889a8bff65ecaf8600c489be04ff40b440e73e29
killgriff22 <kilgrif22033@gmail.com>
2022-07-10 06:34:14 -0700

    inital commit

---
+++ main.py
@@ -0,0 +1,33 @@
+from bs4 import BeautifulSoup
+import flask, requests
+from flask import Flask
+app = Flask(__name__)
+mirroraddr=""
+@app.route('/')
+def index():
+    with open("index.htm", "r") as f:
+        return f.read()
+@app.route('/index.js')
+def idxjs():
+    with open("index.js", "r") as f:
+        return f.read()
+@app.route('/mirror')
+def mirror():
+    global mirroraddr
+    try:
+        mirroraddr = flask.request.args.get('mirroraddr')
+        if not "http://" in mirroraddr:
+            mirroraddr = "http://" + mirroraddr
+        elif not "https://" in mirroraddr:
+            mirroraddr = "https://" + mirroraddr
+        baseaddr = mirroraddr.split("/")[2]
+        content = requests.get(mirroraddr).text
+        soup = BeautifulSoup(content, "html.parser")
+        print(soup.prettify())
+        content.replace("=\"/","=\"https://"+baseaddr+"/")
+    except Exception as e:
+        content = "COULD NOT CONNECT TO SITE TO MIRROR"
+        print(e)
+    return content
+if __name__ == '__main__':
+   app.run(host="0.0.0.0",port=81)
